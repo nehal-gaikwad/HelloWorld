1107077f546e0d22c277e09bcc1a715c11987bbe
nehal-gaikwad <nehal.gaikwad006@gmail.com>
2015-08-19 08:42:42 +0530

    addition program

---
+++ calc.cpp
@@ -0,0 +1,13 @@
+#include <iosream.h>
+using namespace std;
+void main()         //program execution start from here
+{
+cout<<"program for calculator"<<endl;
+int a,b,c;
+cout<<"addition"<<endl;
+cout<<"enter values for a and b"<<endl;
+cin>>a>>b;
+c=a+b;
+cout<<"addition ="<<c;
+return 0;
+}
