6f5b00be04ddccc4d235d84e511dbae334d800db
nehal-gaikwad <nehal.gaikwad006@gmail.com>
2015-08-19 08:45:53 +0530

    add-sub program

--- calc.cpp
+++ calc.cpp
@@ -2,12 +2,19 @@
 using namespace std;
 void main()         //program execution start from here
 {
-cout<<"program for calculator"<<endl;
+cout<<"program for calculator addition & subtraction"<<endl;
 int a,b,c;
-cout<<"addition"<<endl;
+cout<<"1)addition"<<endl;
 cout<<"enter values for a and b"<<endl;
 cin>>a>>b;
 c=a+b;
 cout<<"addition ="<<c;
+
+cout<<"2)subtraction"<<endl;
+cout<<"enter values for a and b"<<endl;
+cin>>a>>b;
+c=a-b;
+cout<<"subtraction ="<<c;
+
 return 0;
 }
