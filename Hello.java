63a20ef4cc07a897e614d1a93a2e7374cc8604e3
nehal-gaikwad <nehal.gaikwad006@gmail.com>
2017-07-20 14:55:16 +0530

    Update Hello.java

--- Hello.java
+++ Hello.java
@@ -1,35 +1,9 @@
+import java.io.*;
+public class Hello{
 
-
-import java.io.IOException;
-import java.io.PrintWriter;
-
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-/**
- * Servlet implementation class Hello
- */
-@WebServlet("/Hello")
-public class Hello extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Hello() {
-        super();
-        // TODO Auto-generated constructor stub
+    public static void main(String[] args) {
+        // Prints "Hello, World" to the terminal window.
+        System.out.println("Hello, World");
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		PrintWriter pw=response.getWriter();
-		pw.print("Hello!!!");
-	}
 
 }
